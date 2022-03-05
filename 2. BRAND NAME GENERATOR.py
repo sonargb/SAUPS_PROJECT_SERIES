530ccae8abc7a864f008191b7cec703289eda175
sonargb <99501671+sonargb@users.noreply.github.com>
2022-03-05 17:27:00 +0530

    Add files via upload

---
+++ 2. BRAND NAME GENERATOR.py
@@ -0,0 +1,5 @@
+print("Hello..! Welcome to our Oraganization.We are here when you need us.")
+a=input("Enter a city in which you grew up: ")
+b=input("Enter your pet name: ")
+c=f"Brand Name is {a}{b}"
+print(c)
